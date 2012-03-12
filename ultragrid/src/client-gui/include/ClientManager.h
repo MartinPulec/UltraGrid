ac8cfb5e1ed32117628af430c105ebc0d6e67273
Martin Pulec <pulec@cesnet.cz>
2012-03-12 17:42:28 +0100

    IN/OUT: removed unused files, added newly used

---
+++ ultragrid/src/client-gui/include/ClientManager.h
@@ -0,0 +1,36 @@
+#ifndef CLIENTMANAGER_H
+#define CLIENTMANAGER_H
+
+#include "../include/sp_client.h"
+#include <string>
+#include <wx/string.h>
+
+class AsyncMsgHandler;
+
+class ClientManager
+{
+    public:
+        ClientManager();
+        virtual ~ClientManager();
+
+        void connect_to(std::string host, int port);
+        void disconnect();
+
+        void set_parameter(wxString val, wxString param);
+        void setup(wxString path);
+        void play(wxString pos = wxEmptyString);
+        void pause(wxString pos = wxEmptyString);
+
+        bool isConnected();
+
+        void teardown();
+
+        void ProcessIncomingData();
+
+        void SetMsgHandler(AsyncMsgHandler *msgHandler);
+    protected:
+    private:
+        sp_client stream_connection;
+};
+
+#endif // CLIENTMANAGER_H
---
+++ ultragrid/src/client-gui/src/ClientManager.cpp
@@ -0,0 +1,154 @@
+#include "../include/ClientManager.h"
+
+#include <stdexcept>
+
+ClientManager::ClientManager()
+{
+    //ctor
+}
+
+ClientManager::~ClientManager()
+{
+    //dtor
+}
+
+void ClientManager::SetMsgHandler(AsyncMsgHandler *msgHandler)
+{
+    stream_connection.SetMsgHandler(msgHandler);
+}
+
+void ClientManager::connect_to(std::string host, int port)
+{
+    stream_connection.connect_to(host, port);
+}
+
+void ClientManager::disconnect()
+{
+    stream_connection.disconnect();
+}
+
+void ClientManager::set_parameter(wxString val, wxString param)
+{
+    wxString msgstr;
+    struct message msg;
+    struct response resp;
+    wxCharBuffer buf;
+
+    msgstr = L"";
+    msgstr << wxT("SET_PARAMETER ") << val << wxT(" ") << param;
+    buf = msgstr.mb_str();
+    msg.msg = buf.data();
+    msg.len = msgstr.Len();
+
+    this->stream_connection.send(&msg, &resp);
+
+    if(resp.code != 200) {
+        wxString msg;
+        msg << wxT("Unable to set video format: ") << resp.code << L" " << wxString::FromUTF8((resp.msg));
+        throw std::runtime_error(std::string(msg.mb_str()));
+    }
+}
+
+void ClientManager::setup(wxString path)
+{
+    wxString msgstr;
+    struct message msg;
+    struct response resp;
+    wxCharBuffer buf;
+
+    msgstr = L"";
+    msgstr << wxT("SETUP ") << path;
+    buf = msgstr.mb_str();
+    msg.msg = buf.data();
+    msg.len = msgstr.Len();
+
+    // TODO: handle TMOUT
+    this->stream_connection.send(&msg, &resp);
+
+    if(resp.code != 201) {
+        wxString msg;
+        msg << wxT("Media setup error: ") << resp.code << L" " << wxString::FromUTF8((resp.msg));
+        throw std::runtime_error(std::string(msg.mb_str()));
+    }
+}
+
+void ClientManager::play(wxString pos)
+{
+    wxString msgstr;
+    struct message msg;
+    struct response resp;
+    wxCharBuffer buf;
+
+    msgstr = L"";
+    msgstr << wxT("PLAY");
+    if(!pos.IsEmpty()) {
+        msgstr << wxT(" ") << pos;
+    }
+    buf = msgstr.mb_str();
+    msg.msg = buf.data();
+    msg.len = msgstr.Len();
+
+    // TODO: handle TMOUT
+    this->stream_connection.send(&msg, &resp);
+
+    if(resp.code != 200) {
+        wxString msg;
+        msg << wxT("Error starting stream: ") << resp.code << L" " << wxString::FromUTF8((resp.msg));
+        throw std::runtime_error(std::string(msg.mb_str()));
+    }
+}
+
+void ClientManager::pause(wxString pos)
+{
+    wxString msgstr;
+    struct message msg;
+    struct response resp;
+    wxCharBuffer buf;
+
+    msgstr = L"";
+    msgstr << wxT("PAUSE");
+    if(!pos.IsEmpty()) {
+        msgstr << wxT(" ") << pos;
+    }
+    buf = msgstr.mb_str();
+    msg.msg = buf.data();
+    msg.len = msgstr.Len();
+
+    // TODO: handle TMOUT
+    this->stream_connection.send(&msg, &resp);
+
+    if(resp.code != 200) {
+        wxString msg;
+        msg << wxT("Error pausing stream: ") << resp.code << L" " << wxString::FromUTF8((resp.msg));
+        throw std::runtime_error(std::string(msg.mb_str()));
+    }
+}
+
+void ClientManager::teardown()
+{
+    wxString msgstr;
+    struct message msg;
+    struct response resp;
+
+    msg.msg = "TEARDOWN";
+    msg.len = strlen(msg.msg);
+
+    // TODO: handle TMOUT
+    this->stream_connection.send(&msg, &resp);
+
+    if(resp.code != 200) {
+        wxString msg;
+        msg << wxT("Error stopping media: ") << resp.code << L" " << wxString::FromUTF8((resp.msg));
+        throw std::runtime_error(std::string(msg.mb_str()));
+    }
+}
+
+void ClientManager::ProcessIncomingData()
+{
+    stream_connection.ProcessIncomingData();
+}
+
+bool ClientManager::isConnected()
+{
+    return stream_connection.isConnected();
+}
