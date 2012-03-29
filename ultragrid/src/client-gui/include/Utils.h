778f061548b6b5cbfd76c2df0bd1f1a2ab3479eb
Martin Pulec <pulec@cesnet.cz>
2012-03-29 20:07:33 +0200

    added some forgotten files

---
+++ ultragrid/src/client-gui/include/Utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <wx/string.h>
+
+class Utils
+{
+    public:
+        Utils();
+        virtual ~Utils();
+
+        static wxString FromCDouble(double value, int precision);
+    protected:
+    private:
+};
+
+#endif // UTILS_H
---
+++ ultragrid/src/client-gui/src/Utils.cpp
@@ -0,0 +1,21 @@
+#include "../include/Utils.h"
+
+Utils::Utils()
+{
+    //ctor
+}
+
+Utils::~Utils()
+{
+    //dtor
+}
+
+
+wxString Utils::FromCDouble(double value, int precision)
+{
+    wxString ret;
+    ret << value;
+    ret.Replace(wxT(","), wxT("."));
+
+    return ret;
+}
