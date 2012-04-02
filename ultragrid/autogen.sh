1386a6440498cbc40a1815a1d490f770b9dcdb8b
Martin Pulec <pulec@cesnet.cz>
2012-04-02 15:23:42 +0200

    updated autogen script - compile UDT and libgpujpeg beforehand

--- ultragrid/autogen.sh
+++ ultragrid/autogen.sh
@@ -1,16 +1,44 @@
 #! /bin/sh
 
+udt_init() {
+        cd ../udt4
+        make clean
+        if [ `uname` -eq "Linux" ]; then
+                if [ `uname -p` -eq "x86_64"]; then
+                        make -e arch=AMD64
+                else
+                        make
+                fi
+        else
+                make -e os=OSX arch=IA32
+        fi
+        cd -
+}
+
+libgpujpeg_init() {
+        cd ../libgpujpeg
+        make clean
+        make
+        cd -
+}
+
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
 cd $srcdir
 
+udt_init
+libgpujpeg_init
+
 aclocal && \
 autoheader && \
 autoconf && \
 $srcdir/configure $@
 STATUS=$?
+
+make clean
 
 cd $ORIGDIR
 
