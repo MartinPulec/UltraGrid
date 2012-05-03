1650b6e7b85aa6d3e400243ccc5aa591ea83c043
Martin Pulec <pulec@cesnet.cz>
2012-05-03 11:07:45 +0200

    added some missing files

---
+++ ultragrid/src/client-gui/include/ClientDataCStr.h
@@ -0,0 +1,18 @@
+#ifndef CLIENTDATACSTR_H
+#define CLIENTDATACSTR_H
+
+#include <wx/clntdata.h>
+
+class ClientDataCStr: public wxClientData
+{
+    public:
+        ClientDataCStr(const char *data);
+        virtual ~ClientDataCStr();
+
+        char *get();
+    protected:
+    private:
+        char *cstring;
+};
+
+#endif // CLIENTDATACSTR_H
---
+++ ultragrid/src/client-gui/src/ClientDataCStr.cpp
@@ -0,0 +1,19 @@
+#include "../include/ClientDataCStr.h"
+
+#include <string.h>
+#include <stdlib.h>
+
+ClientDataCStr::ClientDataCStr(const char *data)
+{
+    this->cstring = strdup(data);
+}
+
+ClientDataCStr::~ClientDataCStr()
+{
+    free(this->cstring);
+}
+
+char *ClientDataCStr::get()
+{
+    return this->cstring;
+}
