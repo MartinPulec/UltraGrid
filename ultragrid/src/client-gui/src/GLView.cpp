ead22a1ee7d11d46d0560c0cce4885b848223334
Martin Pulec <pulec@cesnet.cz>
2012-08-30 02:09:10 +0200

    tmp

--- ultragrid/src/client-gui/src/GLView.cpp
+++ ultragrid/src/client-gui/src/GLView.cpp
@@ -319,7 +319,7 @@ void GLView::PostInit(wxWindowCreateEvent&)
     init_device_shaders();
 
     glGenFramebuffersEXT(1, &fbo_uncompressed);
-    glGenTexture, &texture_uncompressed);
+    glGenTexture(GL_TEXTURE_2D, &texture_uncompressed);
     glBindTexture(GL_TEXTURE_2D, texture_uncompressed);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -844,6 +844,9 @@ void GLView::Render()
             glPushAttrib(GL_VIEWPORT_BIT);
             glViewport( 0, 0, width, height);
 
+            glBindTexture(GL_TEXTURE_2D, texture_display);
+
+            glDrawBuffer(GL_COLOR_ATTACHMENT0_EXT);
             glBegin(GL_QUADS);
             glTexCoord2f(0.0, 0.0); glVertex2f(-1.0, -1.0);
             glTexCoord2f(1.0, 0.0); glVertex2f(1.0, -1.0);
