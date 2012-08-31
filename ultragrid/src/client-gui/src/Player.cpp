5298f086bb03799fc36105996f9ce7aecb4b507b
Martin Pulec <pulec@cesnet.cz>
2012-08-31 10:44:15 +0200

    tmp

--- ultragrid/src/client-gui/src/Player.cpp
+++ ultragrid/src/client-gui/src/Player.cpp
@@ -119,7 +119,7 @@ void Player::Notify()
 
         if(GetCurrentFrame() > buffer.GetUpperBound() - OOB_FRAMES / 2 && // nacetli jsme vice nez pol bufferu
            GetCurrentFrame() < total_frames - OOB_FRAMES) {// nejsme u konce
-            JumpAndPlay(buffer.GetUpperBound() - OOB_FRAMES / 4 );
+            connection.play(buffer.GetUpperBound() - OOB_FRAMES / 4 );
         }
 
 
