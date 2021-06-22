21966b1f97a0edf723d54b0a8a900ffe68f25f31
Martin Pulec <martin.pulec@cesnet.cz>
2021-06-22 13:12:35 +0200

    GitHub CI: sync C/C++ CI with master

--- .github/scripts/macOS/prepare_free_sdks.sh
+++ .github/scripts/macOS/prepare_free_sdks.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -eux
 
+XIMEA_DOWNLOAD_URL=https://www.ximea.com/downloads/recent/XIMEA_OSX_SP.dmg
+
+if [ $# -ge 1 ] && [ x$1 = x-e ]; then
+        curl -LI $XIMEA_DOWNLOAD_URL | grep -i '^etag' | sed 's/.*"\(.*\)".*/\1/'
+        return 0
+fi
+
+rm -rf /var/tmp/sdks-free
 mkdir -p /var/tmp/sdks-free
 cd /var/tmp/sdks-free
-curl -S -LO https://www.ximea.com/downloads/recent/XIMEA_OSX_SP.dmg
+curl -S -LO $XIMEA_DOWNLOAD_URL
 
